c82332cbd1d88642c42fe98b4be1a3e5afc1e673
Vahid <150700164+Vahidsk18@users.noreply.github.com>
2024-07-11 16:54:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// let form=document.querySelector("form")
+
+
+// form.addEventListener("submit",(e)=>{
+//     e.preventDefault();
+    
+//     let ipid=document.querySelector("#loginbar")
+//     console.log(ipid.value)
+//     ipid.value=""
+
+// })
